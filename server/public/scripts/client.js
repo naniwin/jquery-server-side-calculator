63132c24ab785170fd7396ad31c06b239c96d2b0
Jonathan Nguyen <njonathan990@gmail.com>
2022-05-01 20:38:12 -0500

    starting functions for calculations

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,18 @@
 $(document).ready(onReady);
 
+let operator = '';
+
 function onReady() {
-  console.log('hello world!');
+  // operator buttons to click
+  $('.operatorButton').on('click', function () {
+    operator = $(this).html();
+  });
+  // equal button to do calculation
+  $('#equalButton').on('click', inputCalculation);
+}
+
+function inputCalculation() {
+  // obtain number values from input
+  let firstNumber = $('#firstNumInput').val();
+  let secondNumber = $('#secondNumInput').val();
 }
---
+++ server/server.js
@@ -0,0 +1,10 @@
+// requires 
+const express = require('express');
+const app = express();
+// global port to use
+const 5000;
+// spin up server
+app.listen(port,() =>{
+    console.log('server up on: ', port);
+});
+// GET route from history to display
