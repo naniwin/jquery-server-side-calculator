7d032f584e7b51ca1236e1bb365e091d3a264a43
Jonathan Nguyen <njonathan990@gmail.com>
2022-05-02 22:47:14 -0500

    testing server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,4 +15,16 @@ function inputCalculation() {
   // obtain number values from input
   let firstNumber = $('#firstNumInput').val();
   let secondNumber = $('#secondNumInput').val();
+  // route input values to server to calculate
+  // ajax to post route calculation to server.js
+  $.ajax({
+    method: 'POST',
+    url: '/calculation',
+    //   data:
+  }).then((response) => {
+    // obtain calculation from server
+    // post on DOM
+    let result = response.result;
+    $('#result').html(`<h1>${result}</h1>`);
+  });
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 // requires 
 const express = require('express');
+// storing the calculation
+const calculationBox = [];
 const app = express();
+const bodyParser = require('body-parser');
 // global port to use
 const 5000;
 // spin up server
 app.listen(port,() =>{
     console.log('server up on: ', port);
 });
+app.use(express.static('./server/public'));
+app.use(bodyParser.urlencoded({extended:true}));
 // GET route from history to display
+// POST route from input values to calculate
+app.post('/calculation', (req, res)=>{
+    let firstNumber = req.body.firstNumber;
+    let secondNumber = req.body.secondNumber;
+    let result = 0;
+    // calculation using switch/case statements
+    switch(operator){
+        case '+':
+            result = Number(firstNumber) + Number(secondNumber);
+            break;
+        case '-':
+            result = Number(firstNumber) - Number(secondNumber);
+            break;
+    }
+})
