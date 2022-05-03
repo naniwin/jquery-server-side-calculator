6dfdb6ddaf04b8afd4947ba9ec385e342af98c0a
Jonathan Nguyen <njonathan990@gmail.com>
2022-05-02 23:29:38 -0500

    server is up -fixed

--- server/server.js
+++ server/server.js
@@ -1,30 +1,36 @@
-// requires 
+// requires
 const express = require('express');
 // storing the calculation
 const calculationBox = [];
 const app = express();
 const bodyParser = require('body-parser');
 // global port to use
-const 5000;
-// spin up server
-app.listen(port,() =>{
-    console.log('server up on: ', port);
-});
+const port = 5000;
+
 app.use(express.static('./server/public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
 // GET route from history to display
+app.get('/history', (req, res) => {
+  res.send(calculationBox);
+});
+
 // POST route from input values to calculate
-app.post('/calculation', (req, res)=>{
-    let firstNumber = req.body.firstNumber;
-    let secondNumber = req.body.secondNumber;
-    let result = 0;
-    // calculation using switch/case statements
-    switch(operator){
-        case '+':
-            result = Number(firstNumber) + Number(secondNumber);
-            break;
-        case '-':
-            result = Number(firstNumber) - Number(secondNumber);
-            break;
-    }
-})
+app.post('/calculation', (req, res) => {
+  let firstNumber = req.body.firstNumber;
+  let secondNumber = req.body.secondNumber;
+  let result = 0;
+  // calculation using switch/case statements
+  switch (operator) {
+    case '+':
+      result = Number(firstNumber) + Number(secondNumber);
+      break;
+    case '-':
+      result = Number(firstNumber) - Number(secondNumber);
+      break;
+  }
+});
+// spin up server
+app.listen(port, () => {
+  console.log('server up on: ', port);
+});
